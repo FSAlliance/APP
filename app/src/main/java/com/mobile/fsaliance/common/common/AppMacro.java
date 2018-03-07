e8edf4d48cc3a2ee22377a7dccf4c0161e9c4cae
tanyadong <2624073973@qq.com>
2018-03-07 22:07:01 +0800

    主界面切到我的界面，回到主界面数据消失问题

--- app/src/main/java/com/mobile/fsaliance/common/common/AppMacro.java
+++ app/src/main/java/com/mobile/fsaliance/common/common/AppMacro.java
@@ -9,7 +9,6 @@ public class AppMacro {
 	// Crash日志文件夹路径
 	public static final String CRASH_MESSAGE_PATH = APP_PATH + "CrashMeaasge/";
 	public static final String PHOTO_PATH = APP_PATH + "photo/";
-	public static final String REQUEST_URL = "http://221.238.227.82:10086/rest";//接口
 	public static final String REQUEST_IP_PORT = "http://39.107.106.248:3389";//接口http://39.107.106.248:3389
 
 	public static final String REQUEST_GOODS_PATH = "/FSAlliance/rest";//接口
